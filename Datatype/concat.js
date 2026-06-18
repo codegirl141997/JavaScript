734dec7fc365eca00ea7f155a5fa52753d1e1d5d
codegirl1411997 <dhanashreee14@gmail.com>
2026-06-18 20:06:05 +0530

    test

---
+++ Datatype/concat.js
@@ -0,0 +1,23 @@
+
+// what ismean by concat ?
+// Adding two things or  merging two things 
+
+let x =100;
+let y =200;
+
+let a = "radha";
+let b = "shiv";
+
+console.log(x+y); //300
+console.log(a+b); //radhashiv
+
+console.log (x+a); //100radha
+console.log (y+b); //200shiv
+
+console.log(x+y+a+b); //300radhashiv
+console.log("all "+ a+x+y+b);
+
+console.log(a+b+(x+y)); //radhashiv300 
+
+
+
