e0f28907253a222deeb2648335e482711de2f5f9
codegirl1411997 <dhanashreee14@gmail.com>
2026-06-19 20:38:38 +0530

    test

---
+++ Datatype/TemplateLiterals.js
@@ -0,0 +1,52 @@
+//TempalteLiterals: backtick ```(On most keyboards, it is located below the Esc key and to the left of the number 1 key.)
+// String also represent that 
+
+
+let msg = `thsi is JS code`;
+console.log(msg);
+
+
+
+let address = `101 new buildng
+ whitefield, east banglore
+ karantakta, Indoia`;
+
+ console.log(address); // with backtick we can write multiple lines and also paragrapgh 
+
+/*
+ let address1 = "101 new buildngj, shcgs
+ sgcfsu"
+ 
+ */ 
+
+
+
+ console.log(address); 
+
+
+ console.log('the value of a:'+ a);
+ console.log('tje value pof b'+b);
+ console.log('the sum is :'+ (a+b));
+
+let a=100;
+let b =200;
+ console.log(`the value of a is ${a}`); //100
+ console.log(`the value of b is ${b}`); //200
+ console.log(`the sum is = ${a + b}`); //300
+
+
+let name = 'garima' ;
+let age =30;
+let salary = 300000000.35;
+console.log(`the emp name is ${name} and age is${age} and salary is ${salary}`);
+
+console.log('i m learning javascript');
+
+console.log(`'learning javascript'`);
+console.log(" i m `learning` coding");
+
+let username = ' Virat Kohli';
+console.log("//input[text() ='virat kohli']");
+
+console.log(`//input[text()='${username}']`); // This kind of exmaple will be helpful in the webelement 
+
