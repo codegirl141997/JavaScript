a2c8804e68e1c5d99311656eb77aa2c68ffc7076
codegirl1411997 <dhanashreee14@gmail.com>
2026-06-19 19:43:59 +0530

    Commit the new changes

---
+++ Datatype/Mathamatical_Oprator.js
@@ -0,0 +1,23 @@
+console.log(1+1);
+console.log(1-1);
+console.log(10/2);
+console.log(10/3);
+console.log(5/2);
+
+console.log(5.0/2);
+console.log(5.0/2.0);
+console.log(5/2.0);
+
+
+console.log(0/9); // o divided anything is 0
+console.log(9/0); // Infinity 
+console.log(9.2/0); //Infinity
+console.log(9.2/0.0); // Infiniy
+console.log(0/0); // NAn Javascript define its nothing but in java its throw the excpetion 
+console.log(0.0/0.0); //Nan
+
+
+console.log(10%2); //0
+console.log(10%3); //remndier will  be 1
+console.log(100%4); // remndier will  be 1 which will be easy to findout the even or the odd number 
+
